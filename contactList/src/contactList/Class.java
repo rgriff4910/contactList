022b046243f671e3b3f8e12a790681b2be0d681d
rgrif <rgrif@Ronald-Laptop.fios-router.home>
2024-10-21 20:27:44 -0400

    data fields and getter/setter

--- contactList/src/contactList/Class.java
+++ contactList/src/contactList/Class.java
@@ -3,4 +3,20 @@ package contactList;
 public class Class {
 	int number;
 	String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
