2230230803b9ef15d5651cade72bf4c39db147f4
rgrif <rgrif@Ronald-Laptop.fios-router.home>
2024-10-21 20:59:19 -0400

    started method for reading file

--- contactList/src/contactList/Main.java
+++ contactList/src/contactList/Main.java
@@ -1,10 +1,29 @@
 package contactList;
 
 import java.util.Scanner;
+import java.io.File;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
+	private static void read() {
+		List <String> contactList;
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter file name: ");
+		
+		String fileName = input.nextLine();
+		
+		File contactFile = new File(fileName);
+		
+		if(contactFile.exists()) {
+			System.out.print("yes");
+		} else {
+			System.out.print("no");
+		}
+	}
+	
+	public static void main(String[] args) {
 		
 	}
 }
