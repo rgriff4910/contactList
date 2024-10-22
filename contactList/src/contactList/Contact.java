6b945e093e88092f1fa066e4356fa70dbe53fd3b
rgrif <rgrif@Ronald-Laptop.fios-router.home>
2024-10-21 20:40:02 -0400

    comments added to Contact class

--- contactList/src/contactList/Contact.java
+++ contactList/src/contactList/Contact.java
@@ -3,30 +3,38 @@ package contactList;
 public class Contact {
 	int number;
 	String name;
+	// data fields
 	
 	public Contact(String name, int number) {
 		this.name = name;
 		this.number = number;
 	}
+	// constructor
 	
 	public String getName() {
 		return name;
 	}
+	// name getter
 	
 	public void setName(String name) {
 		this.name = name;
 	}
+	// name setter
 	
 	public int getNumber() {
 		return number;
 	}
+	// number getter
 	
 	public void setNumber(int number) {
 		this.number = number;
 	}
+	// number setter
 	
 	@Override
 	public String toString() {
 		return name + " " + number;
 	}
+	//toString method
 }
+// end class Contact
